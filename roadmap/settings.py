8c48f9e4eaa94289b9992009b118164597a19525
Anik Dey <ihanik.ad@gmail.com>
2025-06-23 16:16:37 +0600

    CORS added

--- roadmap/settings.py
+++ roadmap/settings.py
@@ -88,6 +88,7 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -95,6 +96,15 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "https://your-frontend.vercel.app",
+    "https://roadmap-frontend-d4scup2pd-zainicdeys-projects.vercel.app",
+    "https://roadmap-frontend-black.vercel.app/"
+]
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'roadmap.urls'
 
